b731ebf16db4af65df6dd7a2bf8e8d450456211b
ajacker <ajacker@foxmail.com>
2019-11-19 19:03:47 +0800

    更新 55.跳跃游戏（注释说明）

--- 55.跳跃游戏.java
+++ 55.跳跃游戏.java
@@ -7,6 +7,8 @@
 // @lc code=start
 public class Solution {
     public boolean canJump(int[] nums) {
+        //从右往左走。看看能不能跳到最后一个位置
+        //如果b能够跳到终点，a能跳到b，则a也能跳到终点
         int lastPos = nums.length - 1;
         for (int i = nums.length - 1; i >= 0; i--) {
             if (i + nums[i] >= lastPos) {
