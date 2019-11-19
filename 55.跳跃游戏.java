dee8a958afe994e8621f08b37177946e1cedf4a7
ajacker <ajacker@foxmail.com>
2019-11-19 18:57:48 +0800

    更新 55.跳跃游戏 （自底向上的动态规划）

--- 55.跳跃游戏.java
+++ 55.跳跃游戏.java
@@ -6,39 +6,33 @@
 
 // @lc code=start
 class Solution {
-    //记录从某个位置开始是否访问过
+    // 记录从某个位置开始是否访问过
     boolean[] canReach;
-    //记录从某个位置开始是否能够访问到
+    // 记录从某个位置开始是否能够访问到
     boolean[] visited;
 
     public boolean canJump(int[] nums) {
-        if (nums.length == 0) {
-            return false;
-        } else {
-            canReach = new boolean[nums.length];
-            visited = new boolean[nums.length];
-            return canJump(nums, 0);
+        canReach = new boolean[nums.length];
+        visited = new boolean[nums.length];
+        // 最后一个肯定可以跳到最后一个位置
+        visited[nums.length - 1] = true;
+        canReach[nums.length - 1] = true;
+        // 尝试从各个位置跳(从右往左)
+        for (int start = nums.length - 2; start >= 0; start--) {
+            int furthestJump = Integer.min(start + nums[start], nums.length - 1);
+            // 尝试跳跃不同的步长
+            for (int j = start + 1; j <= furthestJump; j++) {
+                // 如果能跳到的地方可达末尾，我也能！
+                if (visited[j] && canReach[j]) {
+                    visited[start] = true;
+                    canReach[start] = true;
+                    break;
+                }
+            }
+
         }
+        return visited[0] && canReach[0];
     }
 
-    public boolean canJump(int[] nums, int start) {
-        if (visited[start])
-            return canReach[start];
-        else
-            visited[start] = true;
-        if (start == nums.length - 1) {
-            canReach[start] = true;
-            return true;
-        }
-        int cur = nums[start];
-        boolean flag = false;
-        // 尝试跳每一种跳法
-        for (int i = cur; i >= 1; i--) {
-            if (start + i < nums.length) {
-                flag = flag || canJump(nums, start + i);
-            }
-        }
-        return canReach[start] = flag;
-    }
 }
 // @lc code=end
