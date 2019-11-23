18156240a687401de5d17078545c9ae0166c5a41
ajacker <ajacker@foxmail.com>
2019-11-23 10:51:32 +0800

    完成 589.N叉树的前序遍历

---
+++ 589.n叉树的前序遍历.java
@@ -0,0 +1,48 @@
+import java.util.ArrayList;
+import java.util.List;
+
+/*
+ * @lc app=leetcode.cn id=589 lang=java
+ *
+ * [589] N叉树的前序遍历
+ */
+
+// @lc code=start
+/*
+// Definition for a Node.
+class Node {
+    public int val;
+    public List<Node> children;
+
+    public Node() {}
+
+    public Node(int _val) {
+        val = _val;
+    }
+
+    public Node(int _val, List<Node> _children) {
+        val = _val;
+        children = _children;
+    }
+};
+*/
+class Solution {
+    List<Integer> result = new ArrayList<>();
+
+    public List<Integer> preorder(Node root) {
+        if (root == null)
+            return result;
+        preOrder(root);
+        return result;
+    }
+
+    public void preOrder(Node root) {
+        result.add(root.val);
+        if(!root.children.isEmpty()){
+            for(Node child:root.children){
+                preorder(child);
+            }
+        }
+    }
+}
+// @lc code=end
