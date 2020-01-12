4a06feb848441b35971d4358d6381e6acfc557a6
ajacker <ajacker@foxmail.com>
2020-01-12 11:45:54 +0800

    完成 704.二分查找

---
+++ 704.二分查找.java
@@ -0,0 +1,24 @@
+/*
+ * @lc app=leetcode.cn id=704 lang=java
+ *
+ * [704] 二分查找
+ */
+
+// @lc code=start
+class Solution {
+    public int search(int[] nums, int target) {
+        int left = 0, right = nums.length;
+        while (left < right) {
+            int mid = (left + right) / 2;
+            if (nums[mid] == target) {
+                return mid;
+            } else if (nums[mid] < target) {
+                left = mid + 1;
+            } else {
+                right = mid;
+            }
+        }
+        return -1;
+    }
+}
+// @lc code=end
