49b8d06838cc55ab759bcf9a4567abef229c456f
ajacker <ajacker@foxmail.com>
2020-01-14 20:36:00 +0800

    完成 709.转换成小写字母

---
+++ 709.转换成小写字母.java
@@ -0,0 +1,14 @@
+/*
+ * @lc app=leetcode.cn id=709 lang=java
+ *
+ * [709] 转换成小写字母
+ */
+
+// @lc code=start
+class Solution {
+    public String toLowerCase(String str) {
+        return str.toLowerCase();
+    }
+}
+// @lc code=end
+
