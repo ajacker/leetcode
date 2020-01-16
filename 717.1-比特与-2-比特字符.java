31d1a1770100354d0d9b5bc2646aa6b768e15aad
ajacker <ajacker@foxmail.com>
2020-01-16 16:35:49 +0800

    完成 717.1比特与2比特字符

---
+++ 717.1-比特与-2-比特字符.java
@@ -0,0 +1,19 @@
+/*
+ * @lc app=leetcode.cn id=717 lang=java
+ *
+ * [717] 1比特与2比特字符
+ */
+
+// @lc code=start
+class Solution {
+    public boolean isOneBitCharacter(int[] bits) {
+        int i = 0;
+        while (i < bits.length - 1) {
+            i += bits[i] + 1;
+        }
+        return i == bits.length - 1;
+
+    }
+}
+// @lc code=end
+
