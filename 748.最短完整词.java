67f0f65f4789917b0bc39c62f1b0dad5b2b42e87
ajacker <ajacker@foxmail.com>
2020-01-23 14:22:18 +0800

    完成 748.最短完整词

---
+++ 748.最短完整词.java
@@ -0,0 +1,37 @@
+/*
+ * @lc app=leetcode.cn id=748 lang=java
+ *
+ * [748] 最短完整词
+ */
+
+// @lc code=start
+class Solution {
+    public String shortestCompletingWord(String licensePlate, String[] words) {
+        int[] target = count(licensePlate);
+        String ans = "";
+        for (String word: words)
+            if ((word.length() < ans.length() || ans.length() == 0) &&
+                    dominates(count(word.toLowerCase()), target))
+                ans = word;
+        return ans;
+    }
+
+    public boolean dominates(int[] count1, int[] count2) {
+        for (int i = 0; i < 26; ++i)
+            if (count1[i] < count2[i])
+                return false;
+        return true;
+    }
+
+    public int[] count(String word) {
+        int[] ans = new int[26];
+        for (char letter: word.toCharArray()){
+            int index = Character.toLowerCase(letter) - 'a';
+            if (0 <= index && index < 26)
+                ans[index]++;
+        }
+        return ans;
+    }
+}
+// @lc code=end
+
