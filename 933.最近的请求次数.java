ca099e76aa49f73fc424098381efe831799e1b63
ajacker <ajacker@foxmail.com>
2020-03-03 10:21:15 +0800

    完成 933.最近的请求次数

---
+++ 933.最近的请求次数.java
@@ -0,0 +1,27 @@
+/*
+ * @lc app=leetcode.cn id=933 lang=java
+ *
+ * [933] 最近的请求次数
+ */
+
+// @lc code=start
+class RecentCounter {
+    Queue<Integer> q;
+
+    public RecentCounter() {
+        q = new LinkedList();
+    }
+
+    public int ping(int t) {
+        q.add(t);
+        while (q.peek() < t - 3000)
+            q.poll();
+        return q.size();
+    }
+}
+
+/**
+ * Your RecentCounter object will be instantiated and called as such:
+ * RecentCounter obj = new RecentCounter(); int param_1 = obj.ping(t);
+ */
+// @lc code=end
