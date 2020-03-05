cb36cbb02ceea090e9fce3cb0222d131ec8d188f
ajacker <ajacker@foxmail.com>
2020-03-05 09:17:36 +0800

    完成 938.二叉搜索树的范围和

---
+++ 938.二叉搜索树的范围和.java
@@ -0,0 +1,27 @@
+/*
+ * @lc app=leetcode.cn id=938 lang=java
+ *
+ * [938] 二叉搜索树的范围和
+ */
+
+// @lc code=start
+/**
+ * Definition for a binary tree node. public class TreeNode { int val; TreeNode
+ * left; TreeNode right; TreeNode(int x) { val = x; } }
+ */
+class Solution {
+    public int rangeSumBST(TreeNode root, int L, int R) {
+        if (root == null) {
+            return 0;
+        }
+        if (root.val < L) {
+            return rangeSumBST(root.right, L, R);
+        }
+        if (root.val > R) {
+            return rangeSumBST(root.left, L, R);
+        }
+        return root.val + rangeSumBST(root.left, L, R) + rangeSumBST(root.right, L, R);
+    }
+
+}
+// @lc code=end
