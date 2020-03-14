112087fbbe316cb4fbc0e02b630b330d3a8146d2
ajacker <ajacker@foxmail.com>
2020-03-14 10:14:44 +0800

    完成 993.二叉树的堂兄弟节点

--- 993.二叉树的堂兄弟节点.java
+++ 993.二叉树的堂兄弟节点.java
@@ -10,25 +10,24 @@
  * left; TreeNode right; TreeNode(int x) { val = x; } }
  */
 class Solution {
+    HashMap<Integer, Integer> depth;
+    HashMap<Integer, TreeNode> parent;
+
     public boolean isCousins(TreeNode root, int x, int y) {
-        HashMap<Integer, Integer> depth;
-        HashMap<Integer, TreeNode> parent;
-    
-        public boolean isCousins(TreeNode root, int x, int y) {
-            depth = new HashMap();
-            parent = new HashMap();
-            dfs(root, null);
-            return (depth.get(x) == depth.get(y) && parent.get(x) != parent.get(y));
+        depth = new HashMap();
+        parent = new HashMap();
+        dfs(root, null);
+        return (depth.get(x) == depth.get(y) && parent.get(x) != parent.get(y));
+    }
+
+    public void dfs(TreeNode node, TreeNode par) {
+        if (node != null) {
+            depth.put(node.val, par != null ? 1 + depth.get(par.val) : 0);
+            parent.put(node.val, par);
+            dfs(node.left, node);
+            dfs(node.right, node);
         }
-    
-        public void dfs(TreeNode node, TreeNode par) {
-            if (node != null) {
-                depth.put(node.val, par != null ? 1 + depth.get(par.val) : 0);
-                parent.put(node.val, par);
-                dfs(node.left, node);
-                dfs(node.right, node);
-            }
-        }
+
     }
 }
 // @lc code=end
