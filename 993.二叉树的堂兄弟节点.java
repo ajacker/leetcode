cc62be2d3613957b67a3f0325b252f1252a37908
ajacker <ajacker@foxmail.com>
2020-03-14 10:13:06 +0800

    完成 993.二叉树的堂兄弟节点

---
+++ 993.二叉树的堂兄弟节点.java
@@ -0,0 +1,34 @@
+/*
+ * @lc app=leetcode.cn id=993 lang=java
+ *
+ * [993] 二叉树的堂兄弟节点
+ */
+
+// @lc code=start
+/**
+ * Definition for a binary tree node. public class TreeNode { int val; TreeNode
+ * left; TreeNode right; TreeNode(int x) { val = x; } }
+ */
+class Solution {
+    public boolean isCousins(TreeNode root, int x, int y) {
+        HashMap<Integer, Integer> depth;
+        HashMap<Integer, TreeNode> parent;
+    
+        public boolean isCousins(TreeNode root, int x, int y) {
+            depth = new HashMap();
+            parent = new HashMap();
+            dfs(root, null);
+            return (depth.get(x) == depth.get(y) && parent.get(x) != parent.get(y));
+        }
+    
+        public void dfs(TreeNode node, TreeNode par) {
+            if (node != null) {
+                depth.put(node.val, par != null ? 1 + depth.get(par.val) : 0);
+                parent.put(node.val, par);
+                dfs(node.left, node);
+                dfs(node.right, node);
+            }
+        }
+    }
+}
+// @lc code=end
