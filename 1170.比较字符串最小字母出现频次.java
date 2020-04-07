653108d949849556c557acc69a0c08a89f7796b9
ajacker <ajacker@foxmail.com>
2020-04-07 09:36:53 +0800

    完成 1170.比较字符串最小字母出现频次

---
+++ 1170.比较字符串最小字母出现频次.java
@@ -0,0 +1,14 @@
+/*
+ * @lc app=leetcode.cn id=1170 lang=java
+ *
+ * [1170] 比较字符串最小字母出现频次
+ */
+
+// @lc code=start
+class Solution {
+    public int[] numSmallerByFrequency(String[] queries, String[] words) {
+
+    }
+}
+// @lc code=end
+
