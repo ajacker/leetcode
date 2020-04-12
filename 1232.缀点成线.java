3237c1971321a66172b6ac50a59887490f3f6ecc
ajacker <ajacker@foxmail.com>
2020-04-12 10:50:39 +0800

    完成 1232.缀点成线

---
+++ 1232.缀点成线.java
@@ -0,0 +1,22 @@
+/*
+ * @lc app=leetcode.cn id=1232 lang=java
+ *
+ * [1232] 缀点成线
+ */
+
+// @lc code=start
+class Solution {
+    public boolean checkStraightLine(int[][] coordinates) {
+        int x1 = coordinates[1][0] - coordinates[0][0];
+        int y1 = coordinates[1][1] - coordinates[0][1];
+        for (int i = 2; i < coordinates.length; i++) {
+            int x2 = coordinates[i][0] - coordinates[0][0];
+            int y2 = coordinates[i][1] - coordinates[0][1];
+            if (x1 * y2 != x2 * y1) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
+// @lc code=end
