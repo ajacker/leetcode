e45c5ab720b1acec07088a40cde5f590fd1580b6
ajacker <ajacker@foxmail.com>
2020-05-10 21:19:15 +0800

    完成 994.腐烂的橘子

---
+++ 994.腐烂的橘子.java
@@ -0,0 +1,60 @@
+/*
+ * @lc app=leetcode.cn id=994 lang=java
+ *
+ * [994] 腐烂的橘子
+ */
+
+// @lc code=start
+class Solution {
+    public int orangesRotting(int[][] grid) {
+        int M = grid.length;
+        int N = grid[0].length;
+        Queue<int[]> queue = new LinkedList<>();
+        // count 表示新鲜橘子的数量
+        int count = 0;
+        for (int r = 0; r < M; r++) {
+            for (int c = 0; c < N; c++) {
+                if (grid[r][c] == 1) {
+                    count++;
+                } else if (grid[r][c] == 2) {
+                    // 记录腐烂橘子的坐标
+                    queue.add(new int[] { r, c });
+                }
+            }
+        }
+        // round 表示腐烂的轮数，或者分钟数
+        int round = 0;
+        while (count > 0 && !queue.isEmpty()) {
+            round++;
+            // 保证每次腐烂一轮橘子，不会重复（类似于层序遍历）
+            int n = queue.size();
+            for (int i = 0; i < n; i++) {
+                int[] orange = queue.poll();
+                int r = orange[0];
+                int c = orange[1];
+                if (r - 1 >= 0 && grid[r - 1][c] == 1) {
+                    grid[r - 1][c] = 2;
+                    count--;
+                    queue.add(new int[] { r - 1, c });
+                }
+                if (r + 1 < M && grid[r + 1][c] == 1) {
+                    grid[r + 1][c] = 2;
+                    count--;
+                    queue.add(new int[] { r + 1, c });
+                }
+                if (c - 1 >= 0 && grid[r][c - 1] == 1) {
+                    grid[r][c - 1] = 2;
+                    count--;
+                    queue.add(new int[] { r, c - 1 });
+                }
+                if (c + 1 < N && grid[r][c + 1] == 1) {
+                    grid[r][c + 1] = 2;
+                    count--;
+                    queue.add(new int[] { r, c + 1 });
+                }
+            }
+        }
+        return count > 0 ? -1 : round;
+    }
+}
+// @lc code=end
