e5a43e8e4f429de99dc0caa963dd9d01c359b246
ajacker <ajacker@foxmail.com>
2020-05-10 13:46:21 +0800

    完成 5.最长回文子串

---
+++ 5.最长回文子串.java
@@ -0,0 +1,41 @@
+/*
+ * @lc app=leetcode.cn id=5 lang=java
+ *
+ * [5] 最长回文子串
+ */
+
+// @lc code=start
+class Solution {
+    public String longestPalindrome(String s) {
+        if (s == null || s.length() < 2) {
+            return s;
+        }
+        int strLen = s.length();
+        int maxStart = 0;  //最长回文串的起点
+        int maxEnd = 0;    //最长回文串的终点
+        int maxLen = 1;  //最长回文串的长度
+        //代表从i到j的字符串是回文串
+        boolean[][] dp = new boolean[strLen][strLen];
+
+        for (int r = 0; r < strLen; r++) {
+            for (int l = 0; l < r; l++) {
+                //如果1个字或者两个相同字，肯定就是回文串了
+                if (s.charAt(l) == s.charAt(r) && (r - l <= 2 || dp[l + 1][r - 1])) {
+                    dp[l][r] = true;
+                    if (r - l + 1 > maxLen) {
+                        maxLen = r - l + 1;
+                        maxStart = l;
+                        maxEnd = r;
+
+                    }
+                }
+
+            }
+
+        }
+        return s.substring(maxStart, maxEnd + 1);
+        
+    }
+}
+// @lc code=end
+
