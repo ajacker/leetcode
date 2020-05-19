a46727430e68b5aab7510aa1f62db6068d1cabf4
ajacker <ajacker@foxmail.com>
2020-05-19 19:45:06 +0800

    更新 56.合并区间

--- 56.合并区间.java
+++ 56.合并区间.java
@@ -9,33 +9,22 @@ import java.util.Arrays;
 // @lc code=start
 class Solution {
     public int[][] merge(int[][] intervals) {
-        boolean[] disable = new boolean[intervals.length];
-        Arrays.sort(intervals, (x, y) -> x[0] - y[0]);
-        int newLen = intervals.length;
-        for (int i = 0; i < intervals.length; i++) {
-            if (disable[i]) {
-                // 之前被合并过的就跳过
-                continue;
-            }
-            for (int j = i + 1; j < intervals.length; j++) {
-                if (intervals[j][0] > intervals[i][1]) {
-                    break;
-                } else {
-                    // 合并区间
-                    intervals[i][1] = Math.max(intervals[i][1], intervals[j][1]);
-                    disable[j] = true;
-                    newLen--;
-                }
+        // 先按照区间起始位置排序
+        Arrays.sort(intervals, (v1, v2) -> v1[0] - v2[0]);
+        // 遍历区间
+        int[][] res = new int[intervals.length][2];
+        int idx = -1;
+        for (int[] interval : intervals) {
+            // 如果结果数组是空的，或者当前区间的起始位置 > 结果数组中最后区间的终止位置，
+            // 则不合并，直接将当前区间加入结果数组。
+            if (idx == -1 || interval[0] > res[idx][1]) {
+                res[++idx] = interval;
+            } else {
+                // 反之将当前区间合并至结果数组的最后区间
+                res[idx][1] = Math.max(res[idx][1], interval[1]);
             }
         }
-        int[][] res = new int[newLen][2];
-        int j = 0;
-        for (int i = 0; i < intervals.length; i++) {
-            if (!disable[i]) {
-                res[j++] = intervals[i];
-            }
-        }
-        return res;
+        return Arrays.copyOf(res, idx + 1);
     }
 }
 // @lc code=end
