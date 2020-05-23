a5aecd132ff7e0dd96de1d645121b5bef96541f5
ajacker <ajacker@foxmail.com>
2020-05-23 09:25:29 +0800

    更新 77.组合

--- 77.组合.java
+++ 77.组合.java
@@ -10,12 +10,10 @@ import java.util.List;
 class Solution {
     List<List<Integer>> res;
     List<Integer> temp;
-    boolean[] vis;
 
     public List<List<Integer>> combine(int n, int k) {
         res = new ArrayList<>();
         temp = new ArrayList<>();
-        vis = new boolean[n + 1];
         process(n, k, 0, 1);
         return res;
     }
@@ -23,15 +21,12 @@ class Solution {
     public void process(int n, int k, int len, int prev) {
         if (len == k) {
             res.add(new ArrayList<>(temp));
+            return;
         }
         for (int i = prev; i <= n; i++) {
-            if (!vis[i]) {
-                temp.add(i);
-                vis[i] = true;
-                process(n, k, len + 1, i);
-                vis[i] = false;
-                temp.remove(temp.size() - 1);
-            }
+            temp.add(i);
+            process(n, k, len + 1, i + 1);
+            temp.remove(temp.size() - 1);
         }
     }
 }
