be4a2f4a6339303f7c44297bd7dbe4babdc75c18
LiaoJie <ajacker@foxmail.com>
2020-08-06 19:08:03 +0800

    完成 81.搜索旋转排序数组 II

---
+++ 81.搜索旋转排序数组-ii.java
@@ -0,0 +1,70 @@
+/*
+ * @lc app=leetcode.cn id=81 lang=java
+ *
+ * [81] 搜索旋转排序数组 II
+ */
+
+// @lc code=start
+class Solution {
+    int[] nums;
+    int target;
+
+    /**
+     * 二分查找找到在哪里旋转的
+     * 
+     * @param left
+     * @param right
+     * @return
+     */
+    public int find_roate_index(int left, int right) {
+        while (left <= right) {
+            int mid = (left + right) / 2;
+            // System.out.println(left+","+right);
+            // 检查索引是否越界
+            if (mid >= 0 && mid + 1 < nums.length && nums[mid] > nums[mid + 1]) {
+                return mid + 1;
+            } else if (nums[mid] < nums[left]) {
+                right = mid - 1;
+            } else if (nums[mid] > nums[left]) {
+                left = mid + 1;
+            } else {
+                left++;
+            }
+        }
+        return 0;
+    }
+
+    public boolean find_target(int left, int right) {
+        while (left <= right) {
+            int mid = (left + right) / 2;
+            // 检查索引是否越界
+            if (mid >= 0 && mid < nums.length && nums[mid] == target) {
+                return true;
+            } else if (nums[mid] < target) {
+                left = mid + 1;
+            } else {
+                right = mid - 1;
+            }
+        }
+        return false;
+    }
+
+    public boolean search(int[] nums, int target) {
+        int n = nums.length;
+        if (n == 0) {
+            return false;
+        }
+        this.nums = nums;
+        this.target = target;
+        int roate = find_roate_index(0, n - 1);
+        System.out.println(roate);
+        if (roate == 0) {
+            return find_target(0, n - 1);
+        }
+        if (target < nums[0]) {
+            return find_target(roate, n - 1);
+        }
+        return find_target(0, roate - 1);
+    }
+}
+// @lc code=end
